7c838db0df7004c53dd5026304cc111f68bcc773
Gon96923 <73469243+Gon96923@users.noreply.github.com>
2024-02-27 23:44:20 +0000

    Add files via upload

---
+++ TPC3/aaaaaaaaaaaa.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''
+Name 
+
+SYNOPSIS
+
+
+DESCRIPTIONS
+
+FILES:
+
+
+'''
+
+from jjcli import *
+
+__version__ = "0.0.1"
+
+
+def main():
+    print("ola")
+
+def __init__():
+    main()
---
+++ TPC3/makepyproject.py
@@ -0,0 +1,39 @@
+import jinja2
+from glob import glob
+
+mods = glob('*.py')
+
+if len(mods) >= 1:
+    name = mods[0].split('.')[0]
+else:
+    name = input("Modulo?")
+
+pp = jinja2.Template('''
+
+[build-system]
+requires = ["flit_core >=3.2,<4"]
+build-backend = "flit_core.buildapi"
+
+[project]
+name = "{{name}}"
+authors = [
+    {name = "{{autor}}", email = "{{email}}"},
+]
+classifiers = [
+    "License :: OSI Approved :: MIT License",
+]
+requires-python = ">=3.8"
+dynamic = ["version", "description"]
+
+dependencies = [
+    "jjcli"
+]
+
+[project.scripts]
+{{name}} = "{{name}}:main"
+
+                     
+''')
+
+r = pp.render({"name":name, "autor":"Gonçalo","email":"goncalo@gmail.com"})
+print(r)
